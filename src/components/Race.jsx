2b633812109818d674df43ba6a5d1beb3d5c9cb7
신지민 <kahin6105@gmail.com>
2022-02-12 16:37:16 +0900

    [ADD] RaceContainer
    Co-authored-by: HaeSim <anjin309@gmail.com>

---
+++ src/components/Race.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styled from './Race.styled'
+
+function Race() {
+  return (
+    <Styled.RaceContainer>Race</Styled.RaceContainer>
+  )
+}
+
+export default Race
---
+++ src/components/Race.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const RaceContainer = styled.div`
+  width: 800px;
+  height: 80px;
+  box-sizing: border-box;
+  border-bottom: 1px rgba(255, 255, 255, 0.05);
+  background-color: #242835;
+`
+
+
+export default {
+  RaceContainer,
+}
