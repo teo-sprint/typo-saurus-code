8f0b55202d16a9502b634910613642117ff674b5
Koeun Lee <korany@kakao.com>
2022-02-13 14:58:13 +0900

    [STYLE] 배경화면 가운데 올 수 있도록 app 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Result from './pages/Result';
 
 function App() {
   return (
-    <div className='App text-body w-[800px] h-[600px] bg-base-dark'>
-      <Routes>
-        <Route path='/' element={<Intro />} />
-        <Route path='/game' element={<Game />} />
-        <Route path='/result' element={<Result />} />
-      </Routes>
+    <div className='App text-body w-[100vw] h-[100vh] flex justify-center items-center bg-base-bg'>
+      <div className='w-[800px] h-[600px] bg-base-dark'>
+        <Routes>
+          <Route path='/' element={<Intro />} />
+          <Route path='/game' element={<Game />} />
+          <Route path='/result' element={<Result />} />
+        </Routes>
+      </div>
     </div>
   );
 }
