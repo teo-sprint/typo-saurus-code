74d390f69bea063725518868283aedef81a77623
Koeun Lee <korany@kakao.com>
2022-02-13 19:02:16 +0900

    [FEAT] 인트로 화면에 배경음악 추가

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,8 +1,16 @@
+import { Bgm } from '../components/Bgm.jsx';
 import Initial from '../components/Initial/Initial.jsx';
+import intro from '../assets/intro.wav';
+import { Music } from '../components/Music';
 
 function Intro() {
+  const [playing, toggle] = Bgm(intro);
+
   return (
-    <div>
+    <div className='relative'>
+      <div className='absolute z-10 top-[34px] right-[34px] cursor-pointer' onClick={toggle}>
+        <Music playing={playing} />
+      </div>
       <Initial />
     </div>
   );
