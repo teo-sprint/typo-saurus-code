1b71d3de7108bd5f577c864fe6efcf9ccf8de456
Koeun Lee <korany@kakao.com>
2022-02-13 22:32:17 +0900

    [STYLE] 타이포 이벤트 분리

--- src/components/TypoTitle/TypoTitle.jsx
+++ src/components/TypoTitle/TypoTitle.jsx
@@ -2,11 +2,24 @@ import React, { useEffect } from 'react';
 import { showTypo } from '../../util/showTypo.js';
 import Styled from './TypoTitle.styled.js';
 
+const showTypoText =
+  "<div class='typo'>" +
+  "<p class='prompt'> ____ _  _ ____ _____</p>" +
+  "<p class='prompt'>(_  _( \\/ (  _ (  _  )</p>" +
+  "<p class='prompt'> )(  \\  / )___/)(_)(</p>" +
+  "<p class='prompt'> (__) (__)(__) (_____)</p>" +
+  "<p class='prompt'> ___   __   __  __ ____ __  __ ___     ___ _____ ____ ____</p>" +
+  "<p class='prompt'>/ __) /__\\ (  )(  (  _ (  )(  / __)   / __(  _  (  _ ( ___)</p>" +
+  "<p class='prompt'>\\__ \\/(__)\\ )(__)( )   /)(__)(\\__ \\  ( (__ )(_)( )(_) )__) </p>" +
+  "<p class='prompt'>(___(__)(__(______(_)\\_(______(___/   \\___(_____(____(____)</p>" +
+  "<p class='prompt'>                                                              </p>\
+    </div>";
+
 function TypoTitle() {
   useEffect(() => {
     const onPressSpace = (e) => {
       if (e.keyCode !== 32) {
-        showTypo();
+        showTypo(showTypoText);
       }
     };
     document.body.addEventListener('keyup', onPressSpace);
@@ -17,7 +30,7 @@ function TypoTitle() {
   }, []);
 
   useEffect(() => {
-    showTypo();
+    showTypo(showTypoText);
   }, []);
 
   return <Styled.TypoTitle className='terminal' />;
