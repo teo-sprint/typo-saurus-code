1a6b88da8d95255bc341648a7d90d7e1c8ce7315
신지민 <kahin6105@gmail.com>
2022-02-14 22:35:46 +0900

    [DEL] 라인넘버 피버 애니메이션 삭제

--- src/components/Line.styled.js
+++ src/components/Line.styled.js
@@ -7,32 +7,8 @@ const LineContainer = styled.div`
   margin-right: 12px;
 `;
 
-const fever = keyframes`
-  0% {
-    background-color: rgba(36, 40, 53, 1);
-    color : rgba(255, 255, 255, 0.3);
-  }
-
-  50% {
-    background-color: rgba(255, 255, 255, 1);
-    color : rgba(0, 0, 0, 0.3)
-  }
-
-  100% {
-    background-color: rgba(36, 40, 53, 1);
-    color : rgba(255, 255, 255, 0.3);
-  }
-
-`;
-
 const LineNum = styled.div`
   color: rgba(255, 255, 255, 0.3);
-  ${(props) =>
-    props.isFever
-      ? css`
-          animation: ${fever} 0.5s ease-in-out infinite;
-        `
-      : 'none;'};
 `;
 
 export default {
