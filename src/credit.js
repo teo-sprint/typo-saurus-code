10df0daa783f91761fb421464b86db45c9908e79
Koeun Lee <korany@kakao.com>
2022-02-20 13:24:23 +0900

    [ADD] 크레딧에 쓰일 이름 데이터 생성

---
+++ src/credit.js
@@ -0,0 +1,13 @@
+export const TEAM_TSD = [
+  { name: '혜심쓰', link: 'https://github.com/HaeSim', role: 'developer' },
+  { name: '곤이', link: 'https://github.com/korany-lee', role: 'developer' },
+  { name: '빈츠', link: 'https://github.com/ByungyeonKim', role: 'developer' },
+  { name: '웹린이', link: 'https://github.com/superLipbalm', role: 'developer' },
+  { name: '해달', link: 'https://github.com/moonseonyeong', role: 'developer' },
+  { name: '민민', link: 'https://github.com/zimiin', role: 'developer' },
+  { name: '비비', link: 'https://github.com/borachoidev', role: 'developer' },
+  { name: '폴', link: 'https://brunch.co.kr/@heavenlych', role: 'designer' },
+  { name: '테오', link: 'https://velog.io/@teo', role: 'mental_holding' },
+  { name: 'josefpres', role: 'sound' },
+  { name: 'annoyedcactus', role: 'sound' },
+];
