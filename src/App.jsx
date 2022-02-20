d0839bd4e02cc2ea2340f08f304d97d4391d0006
Koeun Lee <korany@kakao.com>
2022-02-20 13:25:03 +0900

    [ADD] 크레딧 페이지 app.jsx에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Intro from './pages/Intro';
 import Game from './pages/Game';
 import Result from './pages/Result';
+import Credit from './pages/Credit';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<Intro />} />
           <Route path='/game' element={<Game />} />
           <Route path='/result' element={<Result />} />
+          <Route path='/credit' element={<Credit />} />
         </Routes>
       </div>
     </div>
